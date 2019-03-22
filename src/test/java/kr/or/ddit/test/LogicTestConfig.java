0fd81739e84db976c471697e049f60d1c942c7e2
mkjin <rjswmtk@gmail.com>
2019-03-22 11:48:07 +0900

    기본 환경 세팅

---
+++ src/test/java/kr/or/ddit/test/LogicTestConfig.java
@@ -0,0 +1,14 @@
+package kr.or.ddit.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+						{"classpath:kr/or/ddit/config/spring/application-context.xml",
+						 "classpath:kr/or/ddit/config/spring/context-datasource.xml",
+						 "classpath:kr/or/ddit/config/spring/context-transaction.xml"})
+public class LogicTestConfig {
+	
+}
