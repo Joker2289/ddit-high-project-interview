15237be3434f677f0e76a2b63ce5d4122a3ff08d
pkh <ykh1762@gmail.com>
2019-03-27 14:31:31 +0900

    채용공고 타일 추가

--- src/test/java/kr/or/ddit/recruit/dao/RecruitDaoImplTest.java
+++ src/test/java/kr/or/ddit/recruit/dao/RecruitDaoImplTest.java
@@ -2,10 +2,13 @@ package kr.or.ddit.recruit.dao;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 
+import kr.or.ddit.recruit.model.RecruitVo;
 import kr.or.ddit.test.LogicTestConfig;
 
 public class RecruitDaoImplTest extends LogicTestConfig{
@@ -31,7 +34,23 @@ public class RecruitDaoImplTest extends LogicTestConfig{
 		assertNotNull(recrCnt);
 	}
 	
-	
+	/**
+	 * 
+	 * Method : testGetAllRecr
+	 * 작성자 : PC19
+	 * 변경이력 :
+	 * Method 설명 : 전체 채용공고 조회 테스트.
+	 */
+	@Test
+	public void testGetAllRecr() {
+		/***Given***/
+
+		/***When***/
+		List<RecruitVo> recrList = recrDao.getAllRecr();
+
+		/***Then***/
+		assertNotNull(recrList);
+	}
 	
 	
 
