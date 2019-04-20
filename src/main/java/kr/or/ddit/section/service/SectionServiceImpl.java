8b39c829ec6c02de2ed0fb1aa46fc75e84613558
joker2289 <joker2289.pjk280230@gmail.com>
2019-04-21 00:08:46 +0900

    section.xml 작성, 등록
    section dao, service 작성
    
    섹션 추가, 수정, 삭제, 섹션 리스트 조회 작성

---
+++ src/main/java/kr/or/ddit/section/service/SectionServiceImpl.java
@@ -0,0 +1,38 @@
+package kr.or.ddit.section.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.section.dao.ISectionDao;
+import kr.or.ddit.section.model.SectionVo;
+
+@Service("sectionService")
+public class SectionServiceImpl implements ISectionService{
+
+	@Resource(name="sectionDao")
+	private ISectionDao sectionDao;
+	
+	@Override
+	public int insert_section(SectionVo vo) {
+		return sectionDao.insert_section(vo);
+	}
+
+	@Override
+	public int update_section(SectionVo vo) {
+		return sectionDao.update_section(vo);
+	}
+
+	@Override
+	public int delete_section(String section_code) {
+		return sectionDao.delete_section(section_code);
+	}
+
+	@Override
+	public List<SectionVo> select_sectionList(String portfolio_code) {
+		return sectionDao.select_sectionList(portfolio_code);
+	}
+
+}
