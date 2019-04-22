fe2eddf0c004ae34b4b39c855b3c0653f394b52d
Koozzang <goo8455@naver.com>
2019-04-22 13:40:51 +0900

    알림 창에서의  알림삭제, 언팔로우 구현

---
+++ src/main/java/kr/or/ddit/alarm/service/AlarmServiceImpl.java
@@ -0,0 +1,59 @@
+package kr.or.ddit.alarm.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.alarm.dao.IAlarmDao;
+import kr.or.ddit.alarm.model.AlarmVo;
+import kr.or.ddit.util.pagination.PaginationVo;
+
+@Service("alarmService")
+public class AlarmServiceImpl implements IAlarmService {
+
+	@Resource(name="alarmDao")
+	private IAlarmDao alarmDao;
+	
+	@Override
+	public int insert_alarmInfo(AlarmVo alarmVo) {
+		return alarmDao.insert_alarmInfo(alarmVo);
+	}
+
+	@Override
+	public int update_alarmStatus(AlarmVo alarmVo) {
+		return alarmDao.update_alarmStatus(alarmVo);
+	}
+
+	@Override
+	public int delete_alarmInfo(String alarm_code) {
+		return alarmDao.delete_alarmInfo(alarm_code);
+	}
+
+	@Override
+	public List<AlarmVo> select_recentAlarmList(PaginationVo paginationVo) {
+		return alarmDao.select_recentAlarmList(paginationVo);
+	}
+
+	@Override
+	public List<AlarmVo> select_previousAlarmList(PaginationVo paginationVo) {
+		return alarmDao.select_previousAlarmList(paginationVo);
+	}
+
+	@Override
+	public int select_newAlarmCount(String mem_id) {
+		return alarmDao.select_newAlarmCount(mem_id);
+	}
+
+	@Override
+	public List<AlarmVo> select_nextRecentAlarm(PaginationVo paginationVo) {
+		return alarmDao.select_nextRecentAlarm(paginationVo);
+	}
+
+	@Override
+	public List<AlarmVo> select_nextPreviousAlarm(PaginationVo paginationVo) {
+		return alarmDao.select_nextPreviousAlarm(paginationVo);
+	}
+	
+}
---
+++ src/main/java/kr/or/ddit/alarm/service/IAlarmService.java
@@ -0,0 +1,89 @@
+package kr.or.ddit.alarm.service;
+
+import java.util.List;
+
+import kr.or.ddit.alarm.model.AlarmVo;
+import kr.or.ddit.util.pagination.PaginationVo;
+
+public interface IAlarmService {
+	/**
+	 * Method : insert_alarmInfo
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param alarmVo
+	 * @return
+	 * Method 설명 : 알림 등록
+	 */
+	int insert_alarmInfo(AlarmVo alarmVo);
+	
+	/**
+	 * Method : update_alarmStatus
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param alarmVo
+	 * @return
+	 * Method 설명 : 알림 확인
+	 */
+	int update_alarmStatus(AlarmVo alarmVo);
+	
+	/**
+	 * Method : delete_alarmInfo
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param alarm_code
+	 * @return
+	 * Method 설명 : 알림 삭제
+	 */
+	int delete_alarmInfo(String alarm_code);
+	
+	/**
+	 * Method : select_recentAlarmList
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param mem_id
+	 * @return
+	 * Method 설명 : 최근(일주일) 알림 조회
+	 */
+	List<AlarmVo> select_recentAlarmList(PaginationVo paginationVo);
+	
+	/**
+	 * Method : select_nextRecentAlarm
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param paginationVo
+	 * @return
+	 * Method 설명 : 최근(일주일) 알림 다음페이지
+	 */
+	List<AlarmVo> select_nextRecentAlarm(PaginationVo paginationVo);
+	
+	/**
+	 * Method : select_previousAlarmList
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param mem_id
+	 * @return
+	 * Method 설명 : 이전 알림 조회
+	 */
+	List<AlarmVo> select_previousAlarmList(PaginationVo paginationVo);
+	
+	/**
+	 * Method : select_nextPreviousAlarm
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param paginationVo
+	 * @return
+	 * Method 설명 : 이전 알림 다음페이지
+	 */
+	List<AlarmVo> select_nextPreviousAlarm(PaginationVo paginationVo);
+	
+	/**
+	 * Method : select_newAlarmCount
+	 * 작성자 : goo84
+	 * 변경이력 :
+	 * @param mem_id
+	 * @return
+	 * Method 설명 : 새 알람 갯수
+	 */
+	int select_newAlarmCount(String mem_id);
+	
+}
