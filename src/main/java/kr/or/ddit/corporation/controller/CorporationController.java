2b05cfaed7251e0995172bc3c424e11931c60101
KHK <gusrl56@gmail.com>
2019-04-23 16:37:51 +0900

    파일 업로드

--- src/main/java/kr/or/ddit/corporation/controller/CorporationController.java
+++ src/main/java/kr/or/ddit/corporation/controller/CorporationController.java
@@ -1,9 +1,15 @@
 package kr.or.ddit.corporation.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Matcher;
@@ -12,6 +18,7 @@ import java.util.regex.Pattern;
 import javax.annotation.Resource;
 import javax.mail.Session;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -25,6 +32,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.ddit.career_info.model.Career_infoVo;
@@ -220,7 +228,84 @@ public class CorporationController {
 		insertCnt = postService.insert_post(insertPost);
 		
 		return "redirect:" + request.getContextPath() + "/corporation";
+	}	
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * 이미지 업로드
+	 */
+	@RequestMapping(value = "/fileUpload") // method = RequestMethod.GET 
+	public Map fileUpload(HttpServletRequest req, HttpServletResponse rep) { 
+		//파일이 저장될 path 설정 
+		String path = "d://aaa";
+		Map returnObject = new HashMap(); 
+		
+		try { 
+			// MultipartHttpServletRequest 생성
+			MultipartHttpServletRequest mhsr = (MultipartHttpServletRequest) req; 
+			Iterator iter = mhsr.getFileNames(); 
+			MultipartFile mfile = null; String fieldName = "";
+			List resultList = new ArrayList(); 
+			
+			// 디레토리가 없다면 생성 
+			File dir = new File(path); 
+			if (!dir.isDirectory()) { 
+				dir.mkdirs();
+			} 
+			
+			// 값이 나올때까지 
+			while (iter.hasNext()) { 
+//				fieldName = iter.next(); // 내용을 가져와서 
+				mfile = mhsr.getFile(fieldName); 
+				String origName; 
+				origName = new String(mfile.getOriginalFilename().getBytes("8859_1"), "UTF-8"); //한글꺠짐 방지 
+				
+				// 파일명이 없다면 
+				if ("".equals(origName)) { 
+					continue; 
+				} 
+				
+				// 파일 명 변경(uuid로 암호화) 
+				String ext = origName.substring(origName.lastIndexOf('.')); // 확장자 
+				String saveFileName = getUuid() + ext; 
+				
+				// 설정한 path에 파일저장 
+				File serverFile = new File(path + File.separator + saveFileName);
+				mfile.transferTo(serverFile); 
+					
+				Map file = new HashMap(); 
+				file.put("origName", origName); 
+				file.put("sfile", serverFile); 
+				resultList.add(file); 
+			} 
+			
+			returnObject.put("files", resultList); 
+			returnObject.put("params", mhsr.getParameterMap()); 
+			} catch (UnsupportedEncodingException e) { 
+				// TODO Auto-generated catch block 
+				e.printStackTrace(); 
+			}catch (IllegalStateException e) { 
+				// TODO Auto-generated catch block 
+				e.printStackTrace(); 
+			} catch (IOException e) {
+				// TODO Auto-generated catch block 
+				e.printStackTrace(); 
+				
+			} 
+			return null; 
+			
+	} 
+	
+	//uuid생성 
+	public static String getUuid() { 
+		return UUID.randomUUID().toString().replaceAll("-", ""); 
+		
 	}
+			
+	
+	
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////
 
 	/**
 	 * 타임라인 동영상 url입력
