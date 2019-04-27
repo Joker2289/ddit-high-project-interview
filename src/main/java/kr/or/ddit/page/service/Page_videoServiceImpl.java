61182f1aceb4986435fd16615f9f11164a02fe1f
joker2289 <joker2289.pjk280230@gmail.com>
2019-04-27 17:01:44 +0900

    page_video 테이블 추가

---
+++ src/main/java/kr/or/ddit/page/service/Page_videoServiceImpl.java
@@ -0,0 +1,28 @@
+package kr.or.ddit.page.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.page.dao.IPage_videoDao;
+import kr.or.ddit.page.model.Page_videoVo;
+
+@Service("page_videoService")
+public class Page_videoServiceImpl implements IPage_videoService{
+
+	@Resource(name="page_videoDao")
+	private IPage_videoDao videoDao;
+	
+	@Override
+	public int insert_page_video(Page_videoVo vo) {
+		return videoDao.insert_page_video(vo);
+	}
+
+	@Override
+	public List<Page_videoVo> select_page_video(String page_code) {
+		return videoDao.select_page_video(page_code);
+	}
+
+}
