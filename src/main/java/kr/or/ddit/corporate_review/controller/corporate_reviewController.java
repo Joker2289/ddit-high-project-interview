1a35b697f9e9af94337be65e850c48c9d7a1dc58
PC09 <PC09@PC09-PC>
2019-04-30 17:25:55 +0900

    기업 정보 - 기업리뷰 구현 완료

--- src/main/java/kr/or/ddit/corporate_review/controller/corporate_reviewController.java
+++ src/main/java/kr/or/ddit/corporate_review/controller/corporate_reviewController.java
@@ -15,6 +15,7 @@ import kr.or.ddit.corporation.model.CorporationVo;
 import kr.or.ddit.corporation.service.ICorporationService;
 import kr.or.ddit.member.model.MemberVo;
 import kr.or.ddit.personal_connection.service.IPersonal_connectionService;
+import kr.or.ddit.util.pagination.PaginationVo;
 
 @Controller
 public class corporate_reviewController {
@@ -30,19 +31,36 @@ public class corporate_reviewController {
 	
 	
 	@RequestMapping(path={"/companyReview"})
-	public String companyReviewView(String corp_id, Model model) {
+	public String companyReviewView(String corp_id, Model model, PaginationVo paginationVo) {
+		
+		paginationVo.setPageSize(3);
+		paginationVo.setCorp_id(corp_id);
+		
+		
+	    
 		
 		CorporationVo corporationInfo = corporationService.select_corpInfo(corp_id);
-		List<Corporate_reviewVo> crVo = personalService.select_companyReview(corp_id);
+		List<Corporate_reviewVo> crVo = personalService.select_companyReview(paginationVo);
+		if (crVo.size() > 0){
+			int reviewCnt = Integer.parseInt(crVo.get(0).getReviewCnt());
+			int lastPage = reviewCnt / 3 + (reviewCnt%paginationVo.getPageSize() > 0 ? 1 : 0);
+			model.addAttribute("reviewCnt", reviewCnt);
+			model.addAttribute("lastPage", lastPage);
+		}
 		
 		model.addAttribute("crVo", crVo);
 		model.addAttribute("corporationInfo", corporationInfo);
+		model.addAttribute("paginationVo", paginationVo);
+		
 		return "companyReviewTiles";
 	}
 	
 	
 	@RequestMapping(path={"/companyReviewInsert"})
-	public String companyReviewInsert(Corporate_reviewVo reviewVo) {
+	public String companyReviewInsert(Corporate_reviewVo reviewVo, String[] cr_content) {
+		
+		String contents = cr_content[0]+"☆"+cr_content[1]+"☆"+cr_content[2];
+		reviewVo.setCr_contents(contents);
 		
 		logger.debug("dd{}" , reviewVo);
 		personalService.insert_companyReview(reviewVo);
